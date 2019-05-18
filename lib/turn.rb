aafe4400ca25ee90df1812460b53ae19683f3e42
Ashley Muller <serenehopes@gmail.com>
2019-05-18 05:03:41 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,9 +4,9 @@ end
 
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "---------"
+  puts "-" * 11
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "---------"
+  puts "-" * 11
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
