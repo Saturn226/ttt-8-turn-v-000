0b4478efc7fac5f5e686d2fa7097fab4a8b22d82
Ashley Muller <serenehopes@gmail.com>
2019-05-18 05:10:33 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -27,6 +27,7 @@ end
 
 def move(board, index, character="X")
   board[index] = character
+  display_board(board)
 end
 def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board,index)
